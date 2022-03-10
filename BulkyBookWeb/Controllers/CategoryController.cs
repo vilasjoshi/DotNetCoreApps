24fb7c948e04dedd6f241fca5d2474223858cc50
Vilas S Joshi <vilasjoshi222@gmail.com>
2022-03-10 19:05:49 +0530

    Added new functionality- Edit, Update, Delete
    Update.
    - Added new functionality for Edit, Update, Delete category
    - Notification with toaster alert.

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -10,22 +10,130 @@ namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext context;
+        private readonly ApplicationDbContext _context;
 
-        public CategoryController(ApplicationDbContext _context)
+        public CategoryController(ApplicationDbContext context)
         {
-            context = _context;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Category> categories = context.Categories.ToList();
+            IEnumerable<Category> categories = _context.Categories.ToList();
             return View(categories);
         }
 
+        //GET
         public IActionResult Create()
         {
+            
             return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            //custome erro
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomeError", "Display order and name cannot be same");
+            }
+
+            if (category.DisplayOrder < 0)
+            {
+                ModelState.AddModelError("Invalid Details", "Display order cannot be negative.");
+            }
+
+            //validate the values;
+            //is valid model incoming 
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+             
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category Created Successfully";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryById = _context.Categories.Find(id);
+            //var categoryByIdSingle = _context.Categories.SingleOrDefault(u => u.Id == id);
+            //var categoryByIdFirst = _context.Categories.FirstOrDefault( u => u.Id == id);
+            if (categoryById == null)
+                return NotFound();
+
+            return View(categoryById);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            //custome erro
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomeError", "Display order and name cannot be same");
+            }
+
+            if (category.DisplayOrder < 0)
+            {
+                ModelState.AddModelError("Invalid Details", "Display order cannot be negative.");
+            }
+
+            //validate the values;
+            //is valid model incoming 
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category Updated Successfully.";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryById = _context.Categories.Find(id);
+            //var categoryByIdSingle = _context.Categories.SingleOrDefault(u => u.Id == id);
+            //var categoryByIdFirst = _context.Categories.FirstOrDefault( u => u.Id == id);
+            if (categoryById == null)
+                return NotFound();
+
+            return View(categoryById);
+        }
+        //Post
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(int? id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            TempData["success"] = "Category Deleted Successfully.";
+            return RedirectToAction("Index");
         }
 
 
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,7 +14,9 @@ namespace BulkyBookWeb.Models
 
         [Required]
         public string Name { get; set; }
-
+        
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Cannot Be more than 100")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedOn { get; set; } = DateTime.Now;
